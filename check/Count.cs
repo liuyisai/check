e5fb9d7c945e24fef4a3435310ea36e0addf084a
liuyisai <1031348067@qq.com>
2017-02-25 19:13:34 +0800

    统计的导出

--- check/Count.cs
+++ check/Count.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using CCWin;
+using Microsoft.Office.Interop.Excel;
 
 namespace check
 {
@@ -16,6 +17,56 @@ namespace check
         public Count()
         {
             InitializeComponent();
+        }
+
+        private void skinButton1_Click(object sender, EventArgs e)
+        {
+            string fileName = "";
+            string saveFileName = "";
+            SaveFileDialog saveDialog = new SaveFileDialog();
+            saveDialog.DefaultExt = "xls";
+            saveDialog.Filter = "Excel文件|*.xls";
+            saveDialog.FileName = fileName;
+            saveDialog.ShowDialog();
+            saveFileName = saveDialog.FileName;
+            if (saveFileName.IndexOf(":") < 0) return; //被点了取消
+            Microsoft.Office.Interop.Excel.Application xlApp = new Microsoft.Office.Interop.Excel.Application();
+            if (xlApp == null)
+            {
+                MessageBox.Show("无法创建Excel对象，您的电脑可能未安装Excel");
+                return;
+            }
+            Microsoft.Office.Interop.Excel.Workbooks workbooks = xlApp.Workbooks;
+            Microsoft.Office.Interop.Excel.Workbook workbook = workbooks.Add(Microsoft.Office.Interop.Excel.XlWBATemplate.xlWBATWorksheet);
+            Microsoft.Office.Interop.Excel.Worksheet worksheet = (Microsoft.Office.Interop.Excel.Worksheet)workbook.Worksheets[1];//取得sheet1 
+            //写入标题             
+            for (int i = 0; i < skinDataGridView1.ColumnCount; i++)
+            { worksheet.Cells[1, i + 1] = skinDataGridView1.Columns[i].HeaderText; }
+            //写入数值
+            for (int r = 0; r < skinDataGridView1.Rows.Count; r++)
+            {
+                for (int i = 0; i < skinDataGridView1.ColumnCount; i++)
+                {
+                    worksheet.Cells[r + 2, i + 1] = skinDataGridView1.Rows[r].Cells[i].Value;
+                }
+                System.Windows.Forms.Application.DoEvents();
+            }
+            worksheet.Columns.EntireColumn.AutoFit();//列宽自适应
+            MessageBox.Show(fileName + "资料保存成功", "提示", MessageBoxButtons.OK);
+            if (saveFileName != "")
+            {
+                try
+                {
+                    workbook.Saved = true;
+                    workbook.SaveCopyAs(saveFileName);  //fileSaved = true;                 
+                }
+                catch (Exception ex)
+                {//fileSaved = false;                      
+                    MessageBox.Show("导出文件时出错,文件可能正被打开！\n" + ex.Message);
+                }
+            }
+            xlApp.Quit();
+            GC.Collect();//强行销毁           }
         }
     }
 }
